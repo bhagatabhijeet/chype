52b188d7b868e4a1f6b18c8725279bc53f74cc67
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-27 12:25:09 -0800

    add mainpage and update packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import MainPage from "./pages/MainPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/main" component={MainPage} />
+      </Switch>
+    </Router>
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
