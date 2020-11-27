c17577180d3e046e083937f2fa106dc3adadc52f
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-27 14:44:43 -0800

    add unfinished MainPage LoggedInUserCard and Navbar

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CustomNavbar from '../components/Navbar';
+import LoggedInUserCard from '../components/LoggedInUserCard'
+import { Container } from '@material-ui/core';
 
-export default function MainPage(){
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    textAlign:"center"
+  },
+  paper: {
+    padding: theme.spacing(0),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-  return(
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-          {"Main Page"}
-          </Typography>
-      </Container>      
-    </React.Fragment>
+export default function MainPage() {
+  const classes = useStyles();
+
+  return (
+    <>
+    <div className={classes.root}>
+      <CustomNavbar/>
+      <Grid container spacing={3}>
+         <Grid item xs={3}>
+          <Paper className={classes.paper}>
+          <LoggedInUserCard />
+         
+          </Paper>
+        </Grid>
+        <Grid item xs={9}>
+          {/* <Paper className={classes.paper}>xs=3</Paper> */}
+          <Container>
+            
+          </Container>
+        </Grid>
+        
+      </Grid>
+    </div>
+    </>
   );
 }
