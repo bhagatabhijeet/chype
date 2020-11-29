f5cdb94f2225fe73abfdeac486fbb8c3db621cc9
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-11-28 19:47:20 -0800

    setting up node modules, and dependencies

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-git
+require('dotenv')
+.config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const PORT = 3002;
+
+const server = http.createServer(app);
+
+const io = require('socket.io')(server);
+
