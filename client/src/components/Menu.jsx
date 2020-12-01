adeff13a18809518fbab295cf212d637dde0822c
setare mehr <setaremehr89@gmail.com>
2020-12-01 10:58:40 -0800

    create menuBar for settings and sign out

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import IconButton from '@material-ui/core/IconButton';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+export default function MenuBar() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <IconButton
+                aria-label="settings"
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreHorizIcon />
+            </IconButton>
+            <Menu
+                id="fade-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Fade}
+            >
+                <MenuItem onClick={handleClose}>settings</MenuItem>
+                <MenuItem onClick={handleClose}>sign out</MenuItem>
+            </Menu>
+        </div>
+    );
+}
