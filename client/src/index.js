302591010a292972c93749d8fbfe53936a099447
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-05 12:43:15 -0800

    remove ./app import

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import MainPage from "./pages/MainPage";
 import SignUpNew from "./pages/SignUpNew";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import SettingsPage from './pages/SettingsPage';
-
+import HomePage from './pages/HomePage'
+import 'fontsource-roboto';
 
 ReactDOM.render(
   <StrictMode>
     <Router>
       <Switch>
-        <Route exact path="/" component={SignInPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signin" component={SignInPage} />
         <Route path="/signup" component={SignUpNew} />
         <Route path="/main" component={MainPage} />
         <Route path="/settings" component={SettingsPage} />
