872e333ac749199057588c39de823368cd4fb4fc
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-14 12:43:57 -0800

    move authroutes out of /api thus making auth routes as /auth

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const messageRoutes = require('./messageRoutes');
 const roomRoutes = require('./roomRoutes');
 const { requireAuth } = require("../../middlewares/authMiddlewares");
 const { getUser } = require("../../controllers/authController");
 
-router.use('/auth', authRoutes);
+
 router.use('/user', userRoutes);
 router.get("/getuser", requireAuth, getUser);
 router.use("/room", roomRoutes);
