9b65e7b6f63bd84f1bfbd36565cdeb1fe1163ba5
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-12-15 13:53:56 -0800

    Removing settings page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import MainPage from "./pages/MainPage";
 import SignUp from "./pages/SignUp";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignInNew from "./pages/SignInNew";
-import SettingsPage from './pages/SettingsPage';
 import HomePage from './pages/HomePage';
  
 // import ProtectedRoute from "./pages/ProtectedRoutes";
@@ -18,7 +17,6 @@ function App() {
         <Route path="/signin" component={SignInNew} />
         <Route path="/signup" component={SignUp} />
         <Route path="/main" component={MainPage}/>
-        <Route path="/settings" component={SettingsPage} />
         <Route path="*" component={HomePage} />
       </Switch>
     </Router>
