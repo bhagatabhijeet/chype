c2f6056b3b9050431e34057235f113bbbde378b2
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-12-16 17:30:45 -0800

    -Update of user in order to be able to set socket ID, for current socket user is connected to, on the current user -Other misc improvements

--- routes/authRoutes/index.js
+++ routes/authRoutes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { signUp, getUser, signIn } = require('../../controllers/authController');
-const { requireSignIn } = require('../../middlewares/authMiddlewares');
+const { requireSignIn, requireAuth } = require('../../middlewares/authMiddlewares');
 
 router.post('/signUp', signUp);
-router.get('/current',  requireSignIn, getUser);
+router.get('/current',  requireAuth, getUser);
 router.post('/signIn', requireSignIn, signIn);
 
 module.exports = router;
