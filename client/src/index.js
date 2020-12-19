36f62068b8d4a934c5d6c0a7790652d3edb1f655
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-19 00:26:24 -0800

    resolve the serializable error in redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,37 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import App from "./App";
 import "./index.css";
 import "fontsource-roboto";
 import persistedReducer from "./redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const store = configureStore({
-    reducer: persistedReducer,
-    // middleware: [logger],
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 let persistor = persistStore(store);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
