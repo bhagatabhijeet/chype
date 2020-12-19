a18601160d443d1f128c19a4710b6a032c97a020
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-19 05:53:46 -0800

    add 2 style badges

--- client/src/components/FriendUserCard.jsx
+++ client/src/components/FriendUserCard.jsx
@@ -22,8 +22,10 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import DetailsIcon from '@material-ui/icons/Details';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Collapse from "../pages/SettingsPage";
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import PersonIcon from '@material-ui/icons/Person';
 
-const StyledBadge = withStyles((theme) => ({
+const OnlineStyledBadge = withStyles((theme) => ({
     badge: {
         backgroundColor: '#44b700',
         color: '#44b700',
@@ -52,31 +54,61 @@ const StyledBadge = withStyles((theme) => ({
     },
 }))(Badge);
 
+const OfflineStyledBadge = withStyles((theme) => ({
+  badge: {
+      backgroundColor: 'red',
+      color: '#44b700',
+      // boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          borderRadius: '50%',          
+          // border: '1px solid red',
+          content: '""',
+      },
+  }
+}))(Badge);
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
         // width: 360,
-        height: 60,
+        height: 40,
         fontSize: 1,
-        backgroundColor:'white',
+        // backgroundColor:'#007acc',
         color:'white',
-        boxSizing:'border-box'
+        // boxSizing:'border-box',
+        display:'flex',
+        flexDirection:'row',
+        flexWrap:'wrap',
+        alignContent:'center',        
+        width:'100%',
+        margin:0,
+        "&:hover":{
+          backgroundColor:'#4688f1 !important',
+        }
+
     },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
+    // expand: {
+    //     transform: 'rotate(0deg)',
+    //     marginLeft: 'auto',
+    //     transition: theme.transitions.create('transform', {
+    //         duration: theme.transitions.duration.shortest,
+    //     }),
+    // },
+    // expandOpen: {
+    //     transform: 'rotate(180deg)',
+    // },
     avatar: {
-        backgroundColor: red[500],
+        backgroundColor: '#1e1e1e',
         marginBottom: '0',
-        fontSize:10,
-        width:20,
-        height:20,
+        fontSize:8,
+        width:25,
+        height:25,
+        marginRight:5
     },
     icon: {
         marginBottom: '0',
@@ -85,68 +117,69 @@ const useStyles = makeStyles((theme) => ({
         fontSize: 10,
         backgroundColor:'transparent',
         color:'white'
-    }
+    },
+    
+
 }));
 
 export default function FriendUserCard(props) {
     const ReduxUserState = useSelector(state=>state.user);
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = React.useState(null);
+    // const [isSelected,setIsSelected] = React.useState(false);
+    // const [bgColor,setBgColor] =React.useState('#007acc')
 
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
 
+    console.log("PROPS",props.data);
+    console.log(props.data.loggedIn.status);
 
-
-    const selectedUser=(event)=>{
-        const contacts = props.contactList;
-        const existingContact = contacts.filter(entry => entry.email === props.data.email)
-        if(existingContact.length === 0){
-            props.setContactList(contacts.concat(props.data));
-        }
-
+    const localSelectUserHandler =()=>{
+      // setIsSelected(true);
+      // setBgColor("#2ba2ff");
+      props.selectedUserHandler(props.data)
     }
+    
 
     return (
         // <div >
-        <Card style={{ paddingBottom: '1px' }} className={classes.root} role="button" onClick={selectedUser}>
-            <CardContent>
-                <Avatar aria-label="recipe" className={classes.avatar}>
+        <div className={classes.root}  style={{backgroundColor:props.isSelected?"#2ba2ff":"#007acc"}} role="button" onClick={localSelectUserHandler}>
+            {/* <CardContent> */}
+            
+                <Avatar aria-label="recipe" className={classes.avatar} component="span">
                     {`${props.data.firstName[0].toUpperCase()}${props.data.lastName[0].toUpperCase()}`}
                 </Avatar>
-                <Typography color="textSecondary" gutterBottom>
-
+                <Typography>
                     {`${props.data.firstName} ${props.data.lastName}`}
                 </Typography>
-
-            </CardContent>
-            {/* <CardHeader
-          className={classes.header}
-          avatar={
-            <StyledBadge
-              overlap="circle"
+                {props.data.loggedIn.status===true?
+                (
+                  <OnlineStyledBadge overlap="circle"
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
+                vertical: "bottom",
+                horizontal: "right",
               }}
-              variant="dot"
-            >
-              <Avatar aria-label="recipe" className={classes.avatar}>
-              {`${ReduxUserState.firstName[0].toUpperCase()}${ReduxUserState.lastName[0].toUpperCase()}`}
-          </Avatar>
-            </StyledBadge>
-          }
-          action={
-            <IconButton aria-label="settings" className={classes.icon}>
-              <MoreHorizIcon onClick={handleClick} style={{color: 'white'}}/>
-            </IconButton>
-          }
-          title={`${ReduxUserState.firstName} ${ReduxUserState.lastName}`}
-          subheader="This is my Status"
-        /> */}
+              variant="dot">
+                <VerifiedUserIcon style={{fontSize:20}}/>
+                </OnlineStyledBadge>
+                ):
+                (<OfflineStyledBadge overlap="circle"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                variant="dot">
+                  <VerifiedUserIcon style={{fontSize:20}}/>
+                  </OfflineStyledBadge>)
+                }
+                
+                
+               
+           
 
-        </Card>
+        </div>
 
     );
 }
