ec3d0e0cad55bfc4885b469f9fc12d339d60655d
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-19 00:27:21 -0800

    cleanse

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,19 +1,18 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import { persistReducer } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage' 
-import storage from 'redux-persist/lib/storage/session'
-import {userReducer} from './UserReducer';
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage/session";
+import { userReducer } from "./UserReducer";
 // This will shape what the store looks like for us
 // So the key passed into here, will be the root name of the state
 // the value will be the reducer in charge of handling the state for that root name
 
-const rootReducer = combineReducers({  
-  user:userReducer  
+const rootReducer = combineReducers({
+  user: userReducer,
 });
 
 const persistConfig = {
-  key: 'root',
-  storage
+  key: "root",
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
