dd7baf8df897ed345e35afe818e6ec3cf7660b84
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-18 23:18:49 -0800

    simplify and slim the footer

---
+++ client/src/components/MainFooter.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import axios from "axios";
+import Icon from "@material-ui/core/Icon";
+
+const useStyles = makeStyles((theme) => ({
+  footerBottom: {
+    color: "#096dba",
+    marginTop: 10,
+    fontSize: 15,
+    fontFamily: "mono",
+    fontWeight: 500,
+  },
+  span: {
+    margin: 5,
+  },
+}));
+
+export default function MainFooter() {
+  const classes = useStyles();
+  const [forks, setForks] = useState();
+  const [stars, setStars] = useState();
+
+  const githubContent = async () => {
+    const { data } = await axios.get(
+      `https://api.github.com/repos/bhagatabhijeet/chype`
+    );
+    console.log(data);
+    setForks(data.forks_count);
+    setStars(data.stargazers_count);
+  };
+
+  useEffect(() => {
+    githubContent();
+  }, []);
+  return (
+    <footer className={classes.footerBottom}>
+      <div>
+        <Link
+          color="inherit"
+          href="https://github.com/bhagatabhijeet/chype"
+          target="_blank"
+        >
+          Built with ❤ by Chype Team
+          <span className={classes.span}>
+            <Icon
+              className="far fa-star"
+              style={{ fontSize: 12, marginRight: 5 }}
+            />
+            {stars}
+          </span>
+          <span className={classes.span}>
+            <Icon
+              className="fas fa-code-branch"
+              style={{ fontSize: 12, marginRight: 5 }}
+            />
+            {forks}
+          </span>
+        </Link>
+      </div>
+    </footer>
+  );
+}
