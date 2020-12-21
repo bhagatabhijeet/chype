965f7623ae81c605feb6ad2fe0ea810b8024d3f4
setare mehr <setaremehr89@gmail.com>
2020-12-20 17:42:44 -0800

    add github card to home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import chypeTransInverseLogo from "../assets/images/new_inverse_trans.png";
 import chypeTransLogo from "../assets/images/new_trans.png";
 import FunAnim from "../assets/images/fun_animation.gif";
 import Button from "@material-ui/core/Button";
 import "../assets/styles/common.css";
 // import Deck from "../components/animated/Deck";
+import AppsIcon from "@material-ui/icons/Apps";
+import FaceIcon from "@material-ui/icons/Face";
+import socketIcon from "../assets/images/Socket-IO-Logo.png";
+import MUIIcon from "../assets/images/mui-icon.png";
+import ReactIcon from "../assets/images/React-Logo.png";
+import MongoIcon from "../assets/images/mongo-logo.png";
+import GithubIcon from "@material-ui/icons/GitHub";
+import Tooltip from "@material-ui/core/Tooltip";
+import axios from "axios";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import Badge from "@material-ui/core/Badge";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
 
+// import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Card from "@material-ui/core/Card";
+import IconButton from "@material-ui/core/IconButton";
+import Collapse from "@material-ui/core/Collapse";
+
+import { Link as Scroll } from "react-scroll";
 import AppBarMain from "../components/AppBarMain";
 
 import {
@@ -25,51 +44,170 @@ import {
   useTransition,
   Spring,
 } from "react-spring";
-import {Redirect} from "react-router-dom";
-import {useSelector} from "react-redux";
+import { Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   divider: {
-    // Theme Color, or use css color in quote
     backgroundColor: "#dff3fc",
   },
   photoLink: {
     borderRadius: "50%",
   },
+  root: {
+    minHeight: "10vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    textAlign: "center",
+    marginBottom: "0px",
+    borderRadius: "15px",
+    width: "1230px",
+    justifyContent: "center",
+  },
+  footerBottom: {
+    backgroundColor: "#1e1e1e",
+    color: "#ffffff",
+  },
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    height: "180px",
+    justifyContent: "center",
+    alignItems: "stretch",
+  },
+  chyper: {
+    marginBottom: "32px",
+    marginTop: "22px",
+    backgroundColor: "black",
+    color: "white",
+    borderRadius: "10px",
+  },
+  icon: {
+    color: "#fff",
+    fontSize: "1rem",
+  },
+  colorText: {
+    color: "#0d47a1",
+  },
+  container: {
+    textAlign: "center",
+  },
+  goDown: {
+    color: "#0d47a1",
+    fontSize: "4rem",
+  },
+  div: {
+    backgroundColor: "#bbdefb",
+  },
 }));
 
+function Copyright() {
+  return (
+    <Typography variant="body2" style={{ color: "#ffffff", margin: 10 }}>
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://github.com/bhagatabhijeet/chype"
+        target="_blank"
+      >
+        Chype Team
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
 export default function HomePage() {
+  const [forks, setForks] = useState();
+  const [stars, setStars] = useState();
+
+  const githubContent = async () => {
+    const { data } = await axios.get(
+      `https://api.github.com/repos/bhagatabhijeet/chype`
+    );
+    console.log(data);
+    setForks(data.forks_count);
+    setStars(data.stargazers_count);
+  };
+
+  useEffect(() => {
+    githubContent();
+  }, []);
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
+
   const classes = useStyles();
-  const userReduxState = useSelector(state=>state.user);
+  const userReduxState = useSelector((state) => state.user);
 
   if (userReduxState.loggedIn) {
-    return <Redirect to="/main"/>;
+    return <Redirect to="/main" />;
   }
 
   return (
-      <>
-        <AppBarMain signup signin />
-
-
-        <Grid container direction="column" justify="center" alignItems="center">
-          <Grid item xs={12} spacing={0}>
-            <Animdiv val="Hello there!" />
-            <Typography variant="h4" align="center" component="h4">
-              Welcome to Chype!
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Button className="btn-black-white" href="/signup">
-              I want to be a 'Chyper'
-            </Button>
-          </Grid>
-          <Grid item>
-            <Typography variant="h4" align="center" component="h4"></Typography>
-          </Grid>
+    <div className={classes.div}>
+      <AppBarMain signup signin />
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item xs={12} spacing={0}>
+          <Animdiv val="Hello there!" />
+          <Collapse
+            in={checked}
+            {...(checked ? { timeout: 1000 } : {})}
+            collapsedHeight={50}
+          >
+            <div className={classes.container}>
+              <h1 className={classes.title}>
+                Welcome to <br />
+                <span className={classes.colorText}>Chype.</span>
+              </h1>
+              <Scroll to="" smooth={true}>
+                <IconButton>
+                  <AppsIcon className={classes.goDown} />
+                </IconButton>
+              </Scroll>
+            </div>
+          </Collapse>
         </Grid>
-        <HomePageFooter />
-
-      </>
+        <Grid item xs={12}>
+          <Button className={classes.chyper} href="/signup">
+            I want to be a 'Chyper'
+          </Button>
+        </Grid>
+        <Grid item container xs={8}>
+          <Card className={classes.root}>
+            <Grid item xs={12} justify="center">
+              <CardContent className={classes.footerBottom}>
+                <Typography
+                  variant="body2"
+                  style={{ color: "#ffffff", margin: 10 }}
+                >
+                  GitHub {"  "}
+                  <Badge badgeContent={forks} color="primary">
+                    <AccountTreeIcon />
+                  </Badge>
+                  {"  "}
+                  <Badge badgeContent={stars} color="primary">
+                    <StarBorderIcon />
+                  </Badge>
+                </Typography>
+                <Copyright />
+                <Link
+                  color="inherit"
+                  href="https://github.com/bhagatabhijeet/chype"
+                  target="_blank"
+                >
+                  <GithubIcon style={{ fontSize: 34 }} />
+                </Link>
+              </CardContent>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+      <HomePageFooter />
+    </div>
   );
 }
 
@@ -82,15 +220,3 @@ function Animdiv(props) {
   });
   return <animated.h3 style={springprops}>{props.val}</animated.h3>;
 }
-
-// function ShowY(){
-//   const [show, set] = useState(false)
-//   const transitions = useTransition(show, null, {
-//   from: { opacity: 0 },
-//   enter: { opacity: 1 },
-//   leave: { opacity: 0 },
-//   })
-//   return transitions.map(({ item, key, props }) =>
-//   item && <animated.div key={key} style={props}>✌️</animated.div>
-//   )
-// }
