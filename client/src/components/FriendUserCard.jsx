0197ecf176d999e19f00dc694f09681960355658
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-20 22:52:07 -0800

    cleanse , pretty

--- client/src/components/FriendUserCard.jsx
+++ client/src/components/FriendUserCard.jsx
@@ -1,38 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Badge from "@material-ui/core/Badge";
-// import clsx from 'clsx';
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardActions from '@material-ui/core/CardActions';
-// import Collapse from '@material-ui/core/Collapse';
 import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
-import { useSelector } from "react-redux";
-// import MenuBar from './Menu';
-
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
-import {
-  CardHeader,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem,
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import DetailsIcon from "@material-ui/icons/Details";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import Collapse from "../pages/SettingsPage";
+import { socket } from "../pages/MainPage";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
-import PersonIcon from "@material-ui/icons/Person";
-import CancelIcon from "@material-ui/icons/Cancel";
 
 const OnlineStyledBadge = withStyles((theme) => ({
   badge: {
@@ -67,7 +40,6 @@ const OfflineStyledBadge = withStyles((theme) => ({
   badge: {
     backgroundColor: "red",
     color: "#44b700",
-    // boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
     "&::after": {
       position: "absolute",
       top: 0,
@@ -75,7 +47,6 @@ const OfflineStyledBadge = withStyles((theme) => ({
       width: "100%",
       height: "100%",
       borderRadius: "50%",
-      // border: '1px solid red',
       content: '""',
     },
   },
@@ -83,12 +54,9 @@ const OfflineStyledBadge = withStyles((theme) => ({
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // width: 360,
     height: 40,
     fontSize: 1,
-    // backgroundColor:'#007acc',
     color: "white",
-    // boxSizing:'border-box',
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
@@ -99,16 +67,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "#4688f1 !important",
     },
   },
-  // expand: {
-  //     transform: 'rotate(0deg)',
-  //     marginLeft: 'auto',
-  //     transition: theme.transitions.create('transform', {
-  //         duration: theme.transitions.duration.shortest,
-  //     }),
-  // },
-  // expandOpen: {
-  //     transform: 'rotate(180deg)',
-  // },
+
   avatar: {
     backgroundColor: "#1e1e1e",
     marginBottom: "0",
@@ -128,39 +87,44 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function FriendUserCard(props) {
-  const ReduxUserState = useSelector((state) => state.user);
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  // const [isSelected,setIsSelected] = React.useState(false);
-  // const [bgColor,setBgColor] =React.useState('#007acc')
+  const [online, setOnline] = React.useState(false);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  useEffect(() => {
+    socket.on("SIGN_IN", (payload) => {
+      if (payload.id === props.data._id) {
+        setOnline(true);
+        console.log("signed in");
+      }
+    });
+    socket.on("SIGN_OUT", (payload) => {
+      if (payload.id === props.data._id) {
+        setOnline(false);
+        console.log("signed out");
+      }
+    });
+    setOnline(props.data.loggedIn.status);
+  }, []);
 
   console.log("PROPS", props.data);
   console.log(props.data.loggedIn.status);
 
   const localSelectUserHandler = () => {
-    // setIsSelected(true);
-    // setBgColor("#2ba2ff");
     props.selectedUserHandler(props.data);
   };
 
-  const localFriendRemoval =(event)=>{
+  const localFriendRemoval = (event) => {
     event.stopPropagation();
     props.removeHandler(props.data._id);
-  }
+  };
 
   return (
-    // <div >
     <div
       className={classes.root}
       style={{ backgroundColor: props.isSelected ? "#2ba2ff" : "#007acc" }}
       role="button"
       onClick={localSelectUserHandler}
     >
-      {/* <CardContent> */}
       <IconButton
         aria-label="delete"
         component="span"
@@ -185,7 +149,7 @@ export default function FriendUserCard(props) {
       <Typography>
         {`${props.data.firstName} ${props.data.lastName}`}
       </Typography>
-      {props.data.loggedIn.status === true ? (
+      {online ? (
         <OnlineStyledBadge
           overlap="circle"
           anchorOrigin={{
